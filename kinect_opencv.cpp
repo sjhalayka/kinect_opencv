b39c2324574fc28edcf48f3d21f3fab113a1a3eb
Shawn Halayka <sjhalayka@gmail.com>
2017-10-29 17:00:15 -0600

    Add files via upload

--- kinect_opencv.cpp
+++ kinect_opencv.cpp
@@ -3,12 +3,90 @@
 // Get the Kinect SDK 2.0
 // https://www.microsoft.com/en-ca/download/details.aspx?id=44561
 
+int get_colour_frame(Mat &frame_content)
+{
+	IColorFrameSource *pFrameSource = NULL;
+	hr = m_pKinectSensor->get_ColorFrameSource(&pFrameSource);
+
+	if (FAILED(hr))
+	{
+		cout << "get_ColorFrameSource error" << endl;
+		return hr;
+	}
+
+	hr = pFrameSource->OpenReader(&m_pColorFrameReader);
+
+	if (FAILED(hr))
+	{
+		cout << "OpenReader error" << endl;
+		return hr;
+	}
+
+	SafeRelease(pFrameSource);
+
+	IColorFrame* pFrame = NULL;
+	hr = m_pColorFrameReader->AcquireLatestFrame(&pFrame);
+
+	if (FAILED(hr))
+	{
+		cout << "AcquireLatestFrame error" << endl;
+		return hr;
+	}
+
+	INT frame_width, frame_height;
+	IFrameDescription *pDescription;
+	hr = pFrame->get_FrameDescription(&pDescription);
+
+	if (FAILED(hr))
+	{
+		cout << "get_FrameDescription error" << endl;
+		return hr;
+	}
+
+	hr = pDescription->get_Width(&frame_width);
+
+	if (FAILED(hr))
+	{
+		cout << "get_Width error" << endl;
+		return hr;
+	}
+
+	hr = pDescription->get_Height(&frame_height);
+
+	if (FAILED(hr))
+	{
+		cout << "get_Height error" << endl;
+		return hr;
+	}
+
+	SafeRelease(pDescription);
+
+	UINT nBufferSize = 0;
+	BYTE pBuffer = 0;
+	hr = pColorFrame->AccessRawUnderlyingBuffer(&nBufferSize, reinterpret_cast<BYTE**>(&pBuffer));
+
+	if (FAILED(hr))
+	{
+		cout << "AccessRawUnderlyingBuffer error" << endl;
+		return hr;
+	}
+
+	Mat data_m(frame_width, frame_height, CV_8UC4, pBuffer);
+
+	SafeRelease(pColorFrame);
+	SafeRelease(m_pFrameReader);
+
+	frame_content = data_m.clone();
+}
+
+
 int main(void)
 {
 	HRESULT hr;
 
 	IKinectSensor *m_pKinectSensor = NULL;
-	IColorFrameReader *m_pColorFrameReader;
+	IColorFrameReader *m_pFrameReader;
+
 	hr = GetDefaultKinectSensor(&m_pKinectSensor);
 
 	if (FAILED(hr))
@@ -25,65 +103,17 @@ int main(void)
 		return hr;
 	}
 
-	IColorFrameSource *pColorFrameSource = NULL;
-	hr = m_pKinectSensor->get_ColorFrameSource(&pColorFrameSource);
-	
-	if (FAILED(hr))
-	{
-		cout << "get_ColorFrameSource error" << endl;
-		return hr;
-	}
-	
-	hr = pColorFrameSource->OpenReader(&m_pColorFrameReader);
+	Mat frame;
+	get_colour_frame(frame);
 
-	if (FAILED(hr))
-	{
-		cout << "OpenReader error" << endl;
-		return hr;
-	}
-
-	SafeRelease(pColorFrameSource);
-
-	IColorFrame* pColorFrame = NULL;
-	hr = m_pColorFrameReader->AcquireLatestFrame(&pColorFrame);
-
-	if (FAILED(hr))
-	{
-		cout << "AcquireLatestFrame error" << endl;
-		return hr;
-	}
-
-	INT frame_width, frame_height;
-	IFrameDescription *pDescription;
-	pColorFrame->get_FrameDescription(&pDescription);
-	pDescription->get_Width(&frame_width);
-	pDescription->get_Height(&frame_height);
-	SafeRelease(pDescription);
-
-	UINT nBufferSize = 0;
-	BYTE pBuffer = 0;
-	hr = pColorFrame->AccessRawUnderlyingBuffer(&nBufferSize, reinterpret_cast<BYTE**>(&pBuffer));
-
-	if (FAILED(hr))
-	{
-		cout << "AccessRawUnderlyingBuffer error" << endl;
-		return hr;
-	}
-
-	Mat data_m(frame_width, frame_height, CV_8UC4, pBuffer);
-	
-	SafeRelease(pColorFrame);
-	
-
-
-	imshow("data", data_m);
-	imwrite("out.png", data_m);
+	imshow("data", frame);
+	imwrite("out.png", frame);
 
 	waitKey(0);
 
-	
 
-	SafeRelease(m_pColorFrameReader);
+
+
 	m_pKinectSensor->Close();
 	SafeRelease(m_pKinectSensor);
 
